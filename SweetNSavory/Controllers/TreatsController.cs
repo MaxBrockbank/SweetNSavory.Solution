a78c7043edd26818d91fa9fef1226e48d7ad5713
Max Brockbank <maxbrockbank1999@gmail.com>
2021-01-15 13:44:23 -0800

    build read routes for treatscontroller

--- SweetNSavory/Controllers/TreatsController.cs
+++ SweetNSavory/Controllers/TreatsController.cs
@@ -11,4 +11,31 @@ using System.Security.Claims;
 
 namespace SweetNSavory.Controllers
 {
+  public class TreatsController:Controller
+  {
+    private readonly SweetNSavoryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatsController (SweetNSavoryContext db, UserManager<ApplicationUser> userManager)
+    {
+      _db = db;
+      _userManager = userManager;
+    }
+    
+    //Non-Authorized Routes
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.Flavors)
+        .ThenInclude(join=> join.Flavor)
+        .FirstOrDefault(treat=>treat.TreatId == id);
+      return View(thisTreat);
+    }
+    //Authorized Routes
+  }
+
 }
